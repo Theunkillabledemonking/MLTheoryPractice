b6ac639b48290952a0c6df2f62c03dcf41e11a81
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-04-18 19:05:28 +0900

    학습 주석 작성.

--- practive/ml0418.py
+++ practive/ml0418.py
@@ -1,39 +1,67 @@
 import numpy as np
 
+# 특성(feature) 수
 num_feature = 4
+# 샘플(sample) 수
 num_samples = 1000
 
-np.random.seed(5) # 시드값 고정
+# 시드 고정 (난수 고정)
+np.random.seed(42)
 
-X = np.random.rand(num_samples, num_feature) * 2 # 1000 * 4 -> 0~1의 미만 수
+# 입력 데이터 X 생성 (1000,4) 행렬, 0~2 범위의 값
+X = np.random.rand(num_samples, num_feature) * 2
+
+# 진짜 가중치 w_true 생성: (4, 1)
 w_true = np.random.randint(1, 11, (num_feature, 1))
-b_true = np.random.randn() * 0.5 # 1.5 ~ - 0.5 사이의 노이즈 값
 
+# 진짜 편향 b_true 생성: 스칼라 (float 하나)
+b_true = np.random.randn() * 0.5
+
+# 정답 데이터 y 생성: (1000, 1) = (1000,4) @ (4,1) + (스칼라)
+# [벡터와 스칼라 연산 1]: (X @ wtrue) 결과 (1000, 1) + b_true(스칼라) 
 y = X @ w_true + b_true
 
-# w = np.ones(num_feature, 1) # 초기의 w값은 같으면 안됨
+# ------------------------ 학습용 변수 초기화 -----------------------------
+
+# w를 무작위 값으로 초기화 (4, 1) 행렬
 w = np.random.rand(num_feature, 1)
+
+# b를 무작위 값으로 초기화: 스칼라 (float 하나)
 b = np.random.rand()
 
+# gradient 초기화: (4,) 크기 벡터 (나중에 사용하지 않음)
 gradient = np.zeros(num_feature)
+
+# 학습률
 learning_late = 0.01
 
 ####################################################
 
 for _ in range(1000):
 
-    # 예측값 
+    # 예측값 predict_y 계산
+    # [백터와 스칼라 연산2]: (X @ w) 결과 (1000, 1) + b(스칼라)
     predict_y = X @ w + b
 
-    # 오차
+    # 오차(error) 계산: 예측 - 정답
+    # [벡터 연산]: (1000, 1) - (1000, 1) = (1000, 1)
     error = predict_y - y
 
-    # 기울기
+    # 가중치에 대한 기울기 gradient_w 계산
+    # [벡터 연산]: (X.T @ error) -> (4, 1000) @ (1000, 1) = (4,1)
     gradient_w = X.T @ error / num_samples
+    
+    # 편향에 대한 기울기 gradient_b 계산
+    # [벡터 연산]: error.mean() -> (1000, 1)의 평균값 하나
     gradient_b = error.mean()
 
-    # 파라미터 업데이트
+
+    # 파라미터 업데이트 (경사하강)
+    # [벡터 연산]: (4,1) - (4,1)
     w = w - learning_late * gradient_w
+
+    # [스칼라 연산]: (스칼라) - (스칼라)
     b = b - learning_late * gradient_b
 
+# 최종 학습된 파라미터 출력 
 print(f"W: {w}, B: {b}")
