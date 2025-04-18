cdea8d265f53a57358e81be767b1324da11071f7
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-04-18 17:58:50 +0900

    ml 파라미터 학습 업데이트 과정 학습

--- practive/ml0418.py
+++ practive/ml0418.py
@@ -1,12 +1,39 @@
 import numpy as np
 
+num_feature = 4
+num_samples = 1000
 
 np.random.seed(5) # 시드값 고정
 
-x = np.array([[1, 2], [3, 4]])
-y = np.array([[2],[3]])
+X = np.random.rand(num_samples, num_feature) * 2 # 1000 * 4 -> 0~1의 미만 수
+w_true = np.random.randint(1, 11, (num_feature, 1))
+b_true = np.random.randn() * 0.5 # 1.5 ~ - 0.5 사이의 노이즈 값
 
-print(x.shape)
-print(y.shape)
+y = X @ w_true + b_true
 
-print(f"{x}\n{y}\n{x@y}")
+# w = np.ones(num_feature, 1) # 초기의 w값은 같으면 안됨
+w = np.random.rand(num_feature, 1)
+b = np.random.rand()
+
+gradient = np.zeros(num_feature)
+learning_late = 0.01
+
+####################################################
+
+for _ in range(1000):
+
+    # 예측값 
+    predict_y = X @ w + b
+
+    # 오차
+    error = predict_y - y
+
+    # 기울기
+    gradient_w = X.T @ error / num_samples
+    gradient_b = error.mean()
+
+    # 파라미터 업데이트
+    w = w - learning_late * gradient_w
+    b = b - learning_late * gradient_b
+
+print(f"W: {w}, B: {b}")
